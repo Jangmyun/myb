ea25bce77e12444ac4bc3152a0b109d02599b3c7
Jangmyun <cndlsrb2739@gmail.com>
2024-08-14 19:46:13 +0900

    create layout component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route index Component={Home}></Route>
+          <Route path="/" Component={Layout}>
+            <Route index Component={Home} />
+          </Route>
         </Routes>
       </Router>
     </>
