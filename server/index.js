fc9ad36b20e2864b56000a486f76696b6549c8bc
Jangmyun <cndlsrb2739@gmail.com>
2024-08-15 23:28:02 +0900

    node js express server boilerplate

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ success: true, message: "Test" });
+});
+
+app.listen(port, () => {
+  console.log(`Server Running on Port ${port}`);
+});
