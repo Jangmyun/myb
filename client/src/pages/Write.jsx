e935162e41ddc389f64e5572b9c1381102c9e0af
Jangmyun <cndlsrb2739@gmail.com>
2024-08-19 18:15:10 +0900

    browser width에 따라 edit 모드 변경돼서 보이도록 함

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -1,10 +1,33 @@
 import MDEditor from "@uiw/react-md-editor";
+import { useEffect, useState } from "react";
 
 function Write() {
+  const [value, setValue] = useState("");
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <>
-      <h1 className="text-4xl">Hello World!</h1>
-    </>
+    <div className="flex wrap min-h-screen w-full">
+      <div className="w-full">
+        <MDEditor
+          value={value}
+          preview={width < 1024 ? "edit" : "live"}
+          onChange={setValue}
+          textareaProps={{ placeholder: "Please enter Markdown text" }}
+          height={600}
+        />
+      </div>
+    </div>
   );
 }
 
